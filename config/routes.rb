922560b95677103882f9f8435394390fba7813fa
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-08-02 18:26:09 +0900

    delete routing of groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
 end
