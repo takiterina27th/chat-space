e81b69681a32ab3ec3a6ac93476858ade3aa7bc2
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-08-20 17:59:34 +0900

    add append method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,6 +53,7 @@ $(function(){
       .done(function(data){
         console.log(data)
         var html = buildHTML(data);
+        $('.main__messages').append(html);
         $('.main__form__new-message__input-box--text').val('');
         $('.main__form__new-message--submit-btn').prop('disabled', false);
         $('.main__messages').animate({scrollTop: $('.main__messages')[0].scrollHeight}, 'fast');
