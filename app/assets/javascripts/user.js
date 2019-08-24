c30e7f26cc3f859058318d938d8988462b9866ef
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-08-24 13:11:26 +0900

    created html and append

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,15 @@
 $(function() {
+
+  var user_list = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    user_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
