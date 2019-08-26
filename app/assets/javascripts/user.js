2ce2c6739e487b8fef30f51ad1e563084763ea21
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-08-26 19:37:26 +0900

    edit user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,7 +5,7 @@ $(function() {
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.name}">追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     user_list.append(html);
   }
@@ -41,4 +41,13 @@ $(function() {
       alert('ユーザー検索に失敗しました');
     })
   });
+  
+
+    $('#user-search-result').on('click', '.user-search-add', function() {
+      var name = $(this).data("user-name");
+      var user_id = $(this).data("user-id");
+      console.log(user_id)
+      $(this).parent().remove();
+      appendUserToMemberList(name, user_id);
+    });
 });
