712da7fcdf800dec1ba53686094fce4f41eafd26
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-08-30 16:22:09 +0900

    .doneの定義

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,6 +53,7 @@ $(function(){
         dataType: 'json',
         data: {id: last_message_id}
       })
+      .done(function(messages) {
         var insertHTML = '';
         messages.forEach(function(message){
           insertHTML = buildHTML(message);
