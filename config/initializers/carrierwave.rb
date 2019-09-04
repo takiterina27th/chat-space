a202f5e00be97f737ebf934b6fedb7eaed746c6b
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-09-04 15:57:58 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'takiterina-bucket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takiterina-bucket'
 end
